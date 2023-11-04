9e2c46490c768529f47b48b0e3def3cf401f9e91
Jam <77445181+jamjes@users.noreply.github.com>
2023-11-04 11:32:45 +0000

    End of Session 1 - Jam
    1. Created scenes: TechDemo, StartMenu
    2. Imported TMT Essentials Package for Higher Quality Text UI
    3. Added LevelTransitionHandler script to allow switching between scenes

---
+++ JHGameOff2023/Assets/_Scripts/LevelTransitionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransitionHandler : MonoBehaviour
+{
+    public KeyCode trigger;
+    public string targetScene;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(trigger))
+        {
+            SceneManager.LoadScene(targetScene);
+        }
+    }
+}
