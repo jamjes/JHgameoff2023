ac80c5396b7df9900f2d3c592ed4bc510e15855f
Jam <77445181+jamjes@users.noreply.github.com>
2023-11-04 15:11:01 +0000

    2D Jump working

---
+++ JHGameOff2023/Assets/_Scripts/PlayerMovementController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float gravity;
+    public Vector2 velocity;
+    public float jumpVelocity;
+    public float groundHeight = 3.5f;
+    public bool isGrounded;
+    
+    public bool isHoldingJump = false;
+    public float maxHoldJumpTime = .3f;
+    public float holdJumpTimer = 0;
+
+    public float jumpGroundThreshold = 1;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Vector2 pos = transform.position;
+
+        float groundDistance = Mathf.Abs(pos.y - groundHeight);
+
+        if (isGrounded || groundDistance <= jumpGroundThreshold)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isGrounded = false;
+                velocity.y = jumpVelocity;
+                isHoldingJump = true;
+                holdJumpTimer = 0;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isHoldingJump = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        if (!isGrounded)
+        {
+            
+            if (isHoldingJump)
+            {
+                holdJumpTimer += Time.fixedDeltaTime;
+                if (holdJumpTimer >= maxHoldJumpTime)
+                {
+                    isHoldingJump = false;
+                }
+            }
+            
+            pos.y += velocity.y * Time.fixedDeltaTime;
+            if (!isHoldingJump)
+            {
+                velocity.y += gravity * Time.fixedDeltaTime;
+            }
+            
+
+            if (pos.y <= groundHeight)
+            {
+                pos.y = groundHeight;
+                isGrounded = true;
+            }
+        }
+
+        transform.position = pos;
+    }
+
+}
