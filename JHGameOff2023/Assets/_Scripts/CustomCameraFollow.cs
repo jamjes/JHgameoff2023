b6976041d09b5c8ed6aedb5ac095c778ff87deb9
Jam <77445181+jamjes@users.noreply.github.com>
2023-11-26 22:31:25 +0000

    Updated Custom Camera Script
    Now lens size will fade rather than cut when triggered

--- JHGameOff2023/Assets/_Scripts/CustomCameraFollow.cs
+++ JHGameOff2023/Assets/_Scripts/CustomCameraFollow.cs
@@ -6,33 +6,73 @@ using UnityEngine;
 public class CustomCameraFollow : MonoBehaviour
 {
     public CinemachineVirtualCamera _cam;
+    private float mainLens;
+    private float zoomLens;
+    public float zoomForce = .03f;
+    private bool zoomIn = false, zoomOut = false;
+
+
+    private void Start()
+    {
+        mainLens = _cam.m_Lens.OrthographicSize;
+        zoomLens = mainLens - 2;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && !zoomIn)
+        {
+            zoomIn = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && !zoomOut)
+        {
+            zoomOut = true;
+        }
+
+        if (zoomIn && _cam.m_Lens.OrthographicSize > zoomLens)
+        {
+            _cam.m_Lens.OrthographicSize -= zoomForce;
+        }
+        else if (zoomIn && _cam.m_Lens.OrthographicSize <= zoomLens)
+        {
+            zoomIn = false;
+        }
+
+        if (zoomOut && _cam.m_Lens.OrthographicSize < mainLens)
+        {
+            _cam.m_Lens.OrthographicSize += zoomForce;
+        }
+        else if (zoomOut && _cam.m_Lens.OrthographicSize >= mainLens)
+        {
+            zoomOut = false;
+        }
+    }
+
 
     private void OnEnable()
     {
         PlayerCollisionHandler.OnQTEEnter += ZoomIn;
-        //PlayerQTEController.OnQTEWin += ZoomOut;
         PlayerQTEController.OnQTELoss += ZoomOut;
     }
 
     private void OnDisable()
     {
         PlayerCollisionHandler.OnQTEEnter -= ZoomIn;
-        //PlayerQTEController.OnQTEWin -= ZoomOut;
         PlayerQTEController.OnQTELoss -= ZoomOut;
     }
 
     private void ZoomIn()
     {
-        _cam.m_Lens.OrthographicSize = 6;
+        _cam.m_Lens.OrthographicSize = zoomLens;
     }
 
     private void ZoomOut()
     {
-        _cam.m_Lens.OrthographicSize = 10;
+        _cam.m_Lens.OrthographicSize = mainLens;
     }
 
     public void ResetCam()
     {
-        _cam.m_Lens.OrthographicSize = 10;
+        _cam.m_Lens.OrthographicSize = mainLens;
     }
 }
