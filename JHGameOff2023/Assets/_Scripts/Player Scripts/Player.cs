a6d4e14ecdb37d59f8f43b98d835bbf51cab62bc
Jam <77445181+jamjes@users.noreply.github.com>
2023-11-27 01:29:16 +0000

    Testing Particle Effects

--- JHGameOff2023/Assets/_Scripts/Player Scripts/Player.cs
+++ JHGameOff2023/Assets/_Scripts/Player Scripts/Player.cs
@@ -24,6 +24,8 @@ public class Player : MonoBehaviour
     public PlayerMovementController Movement { private set; get; }
     public PlayerAnimationHandler Animation { private set; get; }
 
+    public ParticleSystem LandParticles;
+
     public bool IsDead;
 
     private void Start()
@@ -50,6 +52,7 @@ public class Player : MonoBehaviour
                 }
                 else if (Movement.Rb2d.velocity.x == 0 && Movement.IsWaiting)
                 {
+                    LandParticles.Play();
                     SetState(PlayerState.Landing);
                 }
                 else if (Movement.Rb2d.velocity.x != 0)
