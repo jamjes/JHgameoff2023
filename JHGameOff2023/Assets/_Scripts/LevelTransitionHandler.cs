eabd3e7d6a4ec3cc95b342edbc44ca4b0921ce4d
Jam <77445181+jamjes@users.noreply.github.com>
2023-11-29 12:37:51 +0000

    Pause Screen + Linked Buttons to Functions

--- JHGameOff2023/Assets/_Scripts/LevelTransitionHandler.cs
+++ JHGameOff2023/Assets/_Scripts/LevelTransitionHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +8,7 @@ public class LevelTransitionHandler : MonoBehaviour
 {
     public KeyCode trigger;
     public string targetScene;
+    public GameObject OptionsPanel, MainPanel;
 
     private void OnEnable()
     {
@@ -39,4 +41,59 @@ public class LevelTransitionHandler : MonoBehaviour
     }
 
     #endregion
+
+    public void LoadMainMenu()
+    {
+        Debug.Log("Go To Main Menu");
+    }
+
+    public void LoadMainLevel()
+    {
+        Debug.Log("Load Game from Beginning");
+    }
+
+    public void LoadCredits()
+    {
+        Debug.Log("Go To Credit");
+    }
+
+    public void ToggleMusic()
+    {
+        Debug.Log("Toggle Music");
+    }
+
+    public void ToggleSoundFX()
+    {
+        Debug.Log("Toggle Sound Effects");
+    }
+
+    public void OpenOptionsMenu()
+    {
+        Debug.Log("Open Options Menu");
+        MainPanel.SetActive(false);
+        OptionsPanel.SetActive(true);
+    }
+
+    public void CloseOptionsMenu()
+    {
+        Debug.Log("Close Options Menu");
+        MainPanel.SetActive(true);
+        OptionsPanel.SetActive(false);
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+    public void PauseGame()
+    {
+        Debug.Log("Game is Paused");
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("Resuming Game");
+    }
 }
