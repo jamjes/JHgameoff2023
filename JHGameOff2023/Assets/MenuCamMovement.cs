42234b8edaef2d26375e4c4967c98e94c182dfcd
Jam <77445181+jamjes@users.noreply.github.com>
2023-12-01 11:40:53 +0000

    Menu and Game BG fixed
    Fixed parallax and BG for both scenes

---
+++ JHGameOff2023/Assets/MenuCamMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x + .01f, transform.position.y);
+    }
+}
